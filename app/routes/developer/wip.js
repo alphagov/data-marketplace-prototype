2bc43f8537b275ce96dae3dd412e1488267523db
Joe Lanman <joelanman@gmail.com>
2024-10-29 10:18:29 +0000

    change credentials name

--- app/routes/developer/wip.js
+++ app/routes/developer/wip.js
@@ -32,15 +32,47 @@ router.get('/api-keys/:name', (request, response) => {
     const data = request.session.data
     const appName = request.params.name
 
-    // display notification banner if app was just created
+    // display notification banners
 
     if (data.appCreated) {
         response.locals.appCreated = true
         delete data.appCreated
     }
 
+    if (data.changesSaved) {
+        response.locals.changesSaved = true
+        delete data.changesSaved
+    }
+
     response.locals.app = data.developer.apps.find(app => app.name == appName)
 
     response.render(path + '/view-api-key')
+
+})
+
+router.get('/api-key-name/:name', (request, response) => {
+
+    const data = request.session.data
+    const appName = request.params.name
+
+    response.locals.app = data.developer.apps.find(app => app.name == appName)
+
+    response.render(path + '/api-key-name')
+
+})
+
+router.post('/api-key-name/:name', (request, response) => {
+
+    const data = request.session.data
+    const newAppName = data['app-name']
+    const oldAppName = request.params.name
+
+    let app = data.developer.apps.find(app => app.name == oldAppName)
+
+    app.name = newAppName
+
+    data.changesSaved = true
+
+    response.redirect(path + '/api-keys/' + newAppName)
 
 })
