6e8a38f507e62f58f72e19b244b9d9fd0bc16072
Joe Lanman <joelanman@gmail.com>
2024-11-13 17:32:03 +0000

    api docs

--- app/routes/support/wip.js
+++ app/routes/support/wip.js
@@ -10,13 +10,42 @@ const root = process.cwd()
 const apiSpecFile = fs.readFileSync(root + '/app/data/metadata_management_api.yaml', 'utf8')
 const apiSpec = yaml.parse(apiSpecFile)
 
-router.get('/ipa-spec/:path', (request, response) => {
+router.get('/ipa-spec/:action', (request, response) => {
 
-    const path = '/' + request.params.path
+    const endpoints = {
+        'search-your-data': {
+            title: 'Search your data',
+            path: '/cataloged-resources',
+            method: 'get'
+        },
+        'submit-dataset': {
+            title: 'Submit dataset',
+            path: '/datasets',
+            method: 'post'
+        },
+        'get-dataset': {
+            title: 'Get dataset',
+            path: '/datasets/{dataset-id}',
+            method: 'get'
+        },
+        'update-dataset': {
+            title: 'Update dataset',
+            path: '/datasets/{dataset-id}',
+            method: 'patch'
+        },
+        'delete-dataset': {
+            title: 'Delete dataset',
+            path: '/datasets/{dataset-id}',
+            method: 'delete'
+        }
+    }
 
-    response.locals.path = path
-
-    response.locals.endpoint = apiSpec.paths[path]
+    const endpoint = endpoints[request.params.action]
+    
+    response.locals.title = endpoint.title
+    response.locals.path = endpoint.path
+    response.locals.method = endpoint.method
+    response.locals.endpoint = apiSpec.paths[endpoint.path][endpoint.method]
     response.locals.components = apiSpec.components
 
     response.render('/WIP/support/ipa-spec')
