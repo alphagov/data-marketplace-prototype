f1beb2af4deae3843498eda30155e1f698de4f9e
Joe Lanman <joelanman@gmail.com>
2025-01-08 17:58:04 +0000

    dgu data

--- app/routes/listings/wip-search.js
+++ app/routes/listings/wip-search.js
@@ -3,41 +3,13 @@ const router = govukPrototypeKit.requests.setupRouter('/WIP')
 
 const root = process.cwd()
 
-const catalogue = require(`${root}/app/data/catalogue.json`)
-const organisations = require(`${root}/app/data/organisations.json`)
-const topics = require(`${root}/app/data/topics.json`)
+const searchData = require(`${root}/app/data/dgu.json`)
 
 const log = (input) => {
     console.log(JSON.stringify(input, null, 2))
 }
 
-const getOrganisation = (id) => {
-    return organisations.find((item) => item.id == id)
-}
-
-const getTopics = (input) => {
-
-    const oldTopics = input.split(',').filter((item) => item != "")
-    const newTopics = oldTopics.map((oldTopic => {
-        if (oldTopic == ""){
-            return
-        }
-        const newTopic = topics.find((topic) => topic.id == oldTopic)
-        return newTopic.name
-    }))
-    return newTopics
-}
-
-// to do - cache this
-const searchData = catalogue.map(item => ({
-    title: item.name,
-    type: item.type,
-    access: item.accessRights,
-    topic: getTopics(item.topic),
-    organisation: getOrganisation(item.provider).name,
-    description: item.description,
-    slug: item.name.toLowerCase().replaceAll(' ','-')
-}))
+log(searchData.length)
 
 const searchConfig = {
     sortings: {
@@ -47,49 +19,56 @@ const searchConfig = {
         }
     },
     aggregations: {
-        topic: {
+        theme: {
             title: 'Topic',
-            size: 30,
+            size: 50,
+            sort: "key",
             conjunction: false,
             hide_zero_doc_count: true
         },
         organisation: {
             title: 'Organisation',
-            size: 30,
+            size: 50,
+            sort: "key",
             conjunction: false,
             hide_zero_doc_count: true
         },
         type: {
             title: 'Type',
-            size: 30,
+            size: 2,
             conjunction: false,
             hide_zero_doc_count: true
         },
         access: {
             title: 'Access',
-            size: 30,
+            size: 2,
             conjunction: false,
             hide_zero_doc_count: true
         }
     },
-    searchableFields: ['title', 'description', 'organisation'],
+    searchableFields: ['title', 'summary', 'organisation'],
 }
 
 const itemsjs = require('itemsjs')(searchData, searchConfig)
 
 const processFilters = (aggregations) => {
 
-    const filters = Object.values(aggregations).map((aggregation) => ({
-        title: aggregation.title,
-        items: aggregation.buckets.map((item) => ({
-            text: item.key,
-            value: item.key,
-            checked: item.selected,
-            label: {
-                classes: "govuk-!-font-size-16"
-            }
+    log(aggregations)
+
+    const filters = Object.values(aggregations)
+        .map((aggregation) => ({
+            title: aggregation.title,
+            items: aggregation.buckets
+                .filter((item) => item.key != "")
+                .map((item) => ({
+                    text: item.key,
+                    value: item.key,
+                    checked: item.selected,
+                    label: {
+                        classes: "govuk-!-font-size-16"
+                    }
+                }))
         }))
-    }))
 
     return filters
 }
